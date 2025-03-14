943e5b83ee8ee1ec5234fc9874ee2392ae209a79
Aldhair Lujan Nicolini <aldhajo.1101@gmail.com>
2025-03-13 22:42:09 -0500

    Services

---
+++ src/pages/ServicesPage/servicespage.jsx
@@ -0,0 +1,94 @@
+import React from "react";
+import "./servicespage.css";
+
+export default function ServicesPage() {
+    return (
+        <>
+         <section className={'slider-section'}>
+          <div className={'slider-container'}>
+            <div>
+              <img src="/src/assets/LogoNL.png" alt="Img-slider" />
+            </div>
+            <div className={'slider-text'}>
+              <h1>Lorem ipsum dolor sit amet consectetur</h1>
+              <p>sed do eiusmod tempor incididunt ut labore et dolore magna aliqua</p>
+            </div>
+          </div>
+      </section>
+
+      <div className={'container'}>
+ 
+  <h2 className={'title'}>Lorem ipsum dolor lorem</h2>
+
+  <div className={'content'}>
+    <div className={'card'}>
+      <div className={'card-text'}>
+        <h3 className={'card-title'}>Lorem ipsum dolor sit amet</h3>
+        <ul className={'card-list'}>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+        </ul>
+      </div>
+      <button className={'btn'}>¡Lo quiero!</button>
+    </div>
+
+    <div className={'card'}>
+      <div className={'card-text'}>
+        <h3 className={'card-title'}>Lorem ipsum dolor sit amet</h3>
+        <ul className={'card-list'}>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+        </ul>
+      </div>
+      <button className={'btn'}>¡Lo quiero!</button>
+    </div>
+
+    <div className={'card'}>
+      <div className={'card-text'}>
+        <h3 className={'card-title'}>Lorem ipsum dolor sit amet</h3>
+        <ul className={'card-list'}>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+          <li>Lorem ipsum dolor sit amet consectetur dolor sit amet</li>
+        </ul>
+      </div>
+      <button className={'btn'}>¡Lo quiero!</button>
+    </div>
+  </div>
+  </div>
+  <div className={'icons-section container'}>
+    <h3 className={'icon-title'}>Lorem ipsum dolor lorem ipsum ipsum</h3>
+    <div className={'icons'}>
+      <div className={'icon-block'}>
+        <span className={'icon'}>X</span>
+        <p className={'icon-text'}>Loremanzo</p>
+      </div>
+      <div className={'icon-block'}>
+        <span className={'icon'}>X</span>
+        <p className={'icon-text'}>Loremanzo</p>
+      </div>
+      <div className={'icon-block'}>
+        <span className={'icon'}>X</span>
+        <p className={'icon-text'}>Loremanzo</p>
+      </div>
+      <div className={'icon-block'}>
+        <span className={'icon'}>X</span>
+        <p className={'icon-text'}>Loremanzo</p>
+      </div>
+      <div className={'icon-block'}>
+        <span className={'icon'}>X</span>
+        <p className={'icon-text'}>Loremanzo</p>
+      </div>
+    </div>
+  </div>
+
+
+<section className={'section-4'}>
+          <h2>¿Lorem ipsum dolor sit amet consectetur?</h2>
+          <button type="submit">Lorem ipsum</button>
+      </section>
+</>
+    )
+}
