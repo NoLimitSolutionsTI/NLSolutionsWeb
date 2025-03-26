dc9cfd1a3d3890b7176ac157b469ff6dffdf9381
Aldhair Lujan Nicolini <aldhajo.1101@gmail.com>
2025-03-26 12:53:39 -0500

    pagination carousel

--- src/components/Carousel/Caurosel.jsx
+++ src/components/Carousel/Caurosel.jsx
@@ -4,6 +4,8 @@ import "swiper/css";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
 import '@/styles/components/Carousel/carousel.scss';
+import { useState, useEffect } from "react";
+
 
 
 const projects = [
@@ -27,15 +29,21 @@ const projects = [
     },
 ];
 const Carousel = () => {
+    const [PantallaPequena, setPantallaPequena] = useState(window.innerWidth <= 768);
+    useEffect(() => {
+        const manejarResize = () => setPantallaPequena(window.innerWidth <= 768);
+        window.addEventListener("resize", manejarResize);
+        return () => window.removeEventListener("resize", manejarResize);
+    }, []);
     return (
         <div className="carousel-container container-sm">
             <Swiper
 
-                modules={[Navigation]}
+                modules={[Navigation, Pagination]}
                 spaceBetween={20}
                 loop={true}
                 navigation={{ nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev" }}
-                pagination={{ clickable: true }}
+                pagination={PantallaPequena ?{ clickable: true }: false}
                 breakpoints={{
                     320: { slidesPerView: 1 },
                     768: { slidesPerView: 1 },
@@ -51,7 +59,7 @@ const Carousel = () => {
                                 <p className="card-text">{project.description}</p>
                             </div>
                             <div className="card-footer">
-                                <button key={index} onClick={() => openModal(proyecto)} className="card-button">Más información </button>
+                                <button  className="card-button">Visita la web </button>
                             </div>
                         </div>
                     </SwiperSlide>
