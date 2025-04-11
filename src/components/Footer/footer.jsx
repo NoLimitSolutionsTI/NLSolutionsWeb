cac224ed68a95b21a1d2afbd1adad80cec520a50
Thony <antholeca45@gmail.com>
2025-04-11 22:30:37 +0000

    Actualizacion del Footer

--- src/components/Footer/footer.jsx
+++ src/components/Footer/footer.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faHome, faEnvelope, faPhone, faUsers,faCode} from "@fortawesome/free-solid-svg-icons";
+import { faHome, faEnvelope, faPhone, faUsers,faCode, faCaretDown, faCaretUp} from "@fortawesome/free-solid-svg-icons";
 import { faFacebook, faInstagram, faLinkedin, faTiktok } from "@fortawesome/free-brands-svg-icons";
 
 import '@/styles/components/Footer/footer.scss'
 
 export default function Footer() {
+
+    const [ShowNavFooter, setNavFooter]= useState(window.innerWidth < 1050);
+    const [NavFooterOpen, setNavFooterOpen] = useState(false);
+    const toggle_navFooter = (index) => {
+        setNavFooterOpen((prev) => ({
+          ...prev,
+          [index]: !prev[index], // alterna true/false por índice
+        }));
+      };
+
+    useEffect(()=>{
+        const handleResize= ()=>{
+            setNavFooter(window.innerWidth < 1050);
+            if (window.innerWidth >= 1050) {
+                setNavFooter(false);
+            }
+        }
+        window.addEventListener("resize", handleResize);
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
   return (
     <>
         <footer >
@@ -16,8 +36,8 @@ export default function Footer() {
                     </div>
                     <div className={'nav-container-footer'}>
                         <div className="nav-container">
-                            <h3>Explora</h3>
-                            <ul className="nav-footer">
+                            <span onClick={() => toggle_navFooter(0)}>Explora {ShowNavFooter && (<FontAwesomeIcon icon={NavFooterOpen[0]?faCaretUp: faCaretDown}/>)}</span>
+                            <ul className={`nav-footer ${NavFooterOpen[0] ? "open" : ""}`}>
                                 <li><a href="/"><FontAwesomeIcon icon={faHome} /> Inicio</a></li>
                                 <li><a href="/services"><FontAwesomeIcon icon={faCode} />  Servicios</a></li>
                                 <li><a href="/about"><FontAwesomeIcon icon={faUsers} /> Nosotros</a></li>
@@ -27,8 +47,8 @@ export default function Footer() {
 
                         {/* Sección de Contacto */}
                         <div className="nav-container">
-                            <h3>Contacto</h3>
-                            <ul className="nav-footer">
+                            <span onClick={() => toggle_navFooter(1)}>Contacto {ShowNavFooter && (<FontAwesomeIcon icon={NavFooterOpen[1]?faCaretUp: faCaretDown}/>)}</span>
+                            <ul className={`nav-footer ${NavFooterOpen[1] ? "open" : ""}`}>
                                 <li><a href="mailto:nolimitsolutionsti@gmail.com"><FontAwesomeIcon icon={faEnvelope} /> nolimitsolutionsti@gmail.com</a></li>
                                 <li><a href="tel:+51941778278"><FontAwesomeIcon icon={faPhone} /> +51 941 778 278</a></li>
                             </ul>
@@ -36,8 +56,8 @@ export default function Footer() {
 
                         {/* Sección de Redes Sociales */}
                         <div className="nav-container">
-                            <h3>Síguenos</h3>
-                            <ul className="nav-footer">
+                            <span onClick={() => toggle_navFooter(2)}>Síguenos {ShowNavFooter && (<FontAwesomeIcon icon={NavFooterOpen[2]?faCaretUp: faCaretDown}/>)}</span>
+                            <ul className={`nav-footer ${NavFooterOpen[2] ? "open" : ""}`}>
                                 <li><a href="https://facebook.com" target="_blank"><FontAwesomeIcon icon={faFacebook} /> Facebook</a></li>
                                 <li><a href="https://instagram.com" target="_blank"><FontAwesomeIcon icon={faInstagram} /> Instagram</a></li>
                                 <li><a href="https://linkedin.com" target="_blank"><FontAwesomeIcon icon={faLinkedin} /> LinkedIn</a></li>
@@ -49,7 +69,7 @@ export default function Footer() {
             </div>
 
             <div className={'copyright'}>
-                <h3>© 2025 NoLimitSolutions TI - Todos los derechos reservados</h3>
+                <span>© 2025 NoLimitSolutions TI - Todos los derechos reservados</span>
             </div>
         </footer>
     </>
